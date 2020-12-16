b42f97b406a6880640aae514172430a4279c6a2a
RaptorJung <jk96491@naver.com>
2020-12-16 14:15:56 +0900

    GAN 모델 추가

--- GAN_Model/GAN.py
+++ GAN_Model/GAN.py
@@ -1,5 +1,49 @@
+import torch
+import torch.nn as nn
 from GAN_Model.Discriminator import Discriminator
 from GAN_Model.Generator import Generator
+from torch import optim
+import numpy as np
+from torch.autograd import Variable
 
+
+class gan(nn.Module):
+    def __init__(self, image_shape, args):
+        super(gan, self).__init__()
+        self.image_shape = image_shape
+        self.args = args
+
+        self.Generator = Generator(self.image_shape, self.args)
+        self.Discriminator = Discriminator(self.image_shape)
+
+        self.optimizer_generator = optim.Adam(self.Generator.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+        self.optimizer_discriminator = optim.Adam(self.Discriminator.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+
+        self.adversarial_loss = nn.BCELoss()
+
+    def learn_generator(self, image, valid):
+        z = Variable(torch.FloatTensor(np.random.normal(0, 1, (image.shape[0],  self.args.latent_dim))))
+
+        generator_images = self.Generator(z)
+        discriminator_result = self.Discriminator(generator_images)
+        loss = self.adversarial_loss(discriminator_result, valid)
+
+        self.optimizer_generator.zero_grad()
+        loss.backward()
+        self.optimizer_generator.step()
+
+        return loss.item(), generator_images
+
+    def learn_discriminator(self, real_images, fake, valid, generator_images):
+        real_loss = self.adversarial_loss(self.Discriminator(real_images), valid)
+        fake_loss = self.adversarial_loss(self.Discriminator(generator_images.detach()), fake)
+
+        loss = (real_loss + fake_loss) / 2
+
+        self.optimizer_discriminator.zero_grad()
+        loss.backward()
+        self.optimizer_discriminator.step()
+
+        return loss.item()
 
 
--- GAN_Model/main.py
+++ GAN_Model/main.py
@@ -1,9 +1,37 @@
+import torch
 from GAN_Model.Parser_args import parse_Arg
 from Utils import MnistLoadData
-
+from torch.autograd import Variable
+from GAN_Model.GAN import gan
+from torchvision.utils import save_image
 
 args = parse_Arg()
-
 image_shape = (args.channels, args.image_size, args.image_size)
-
 data_loader = MnistLoadData(args.image_size, args.batch_size)
+
+model = gan(image_shape, args)
+
+for epoch in range(args.n_epochs):
+    for i, (images, _) in enumerate(data_loader):
+
+        valid = Variable(torch.FloatTensor(images.size(0), 1).fill_(1.0), requires_grad=False)
+        fake = Variable(torch.FloatTensor(images.size(0), 1).fill_(0.0), requires_grad=False)
+
+        real_images = Variable(images.type(torch.FloatTensor))
+
+        generator_loss, generator_image = model.learn_generator(images, valid)
+        discriminator_loss = model.learn_discriminator(real_images, fake, valid, generator_image)
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [Discriminator_loss: %f] [Generator_loss: %f]"
+            % (epoch, args.n_epochs, i, len(data_loader), discriminator_loss, generator_loss)
+        )
+
+        batches_done = epoch * len(data_loader) + i
+        if batches_done % args.sample_interval == 0:
+            save_image(generator_image.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+
+
+
+
+
