9e29bec540ba0ceac642aabbe6f6b7b2465505e6
RaptorJung <jk96491@naver.com>
2020-12-18 19:06:40 +0900

    매개변수 수정

--- DCGAN_Model/Discriminator.py
+++ DCGAN_Model/Discriminator.py
@@ -9,22 +9,27 @@ class Discriminator(nn.Module):
 
         super(Discriminator, self).__init__()
 
-        self.layer1 = nn.Sequential(nn.Conv2d(self.channels, self.noise_filter, 4, 2, 1, bias=False),
+        self.layer1 = nn.Sequential(nn.Conv2d(self.channels, self.noise_filter,
+                                              kernel_size=4, stride=2, padding=1, bias=False),
                                     nn.LeakyReLU(0.2, inplace=True))
 
-        self.layer2 = nn.Sequential(nn.Conv2d(self.noise_filter, self.noise_filter * 2, 4, 2, 1, bias=False),
+        self.layer2 = nn.Sequential(nn.Conv2d(self.noise_filter, self.noise_filter * 2,
+                                              kernel_size=4, stride=2, padding=1, bias=False),
                                     nn.BatchNorm2d(self.noise_filter * 2),
                                     nn.LeakyReLU(0.2, inplace=True))
 
-        self.layer3 = nn.Sequential(nn.Conv2d(self.noise_filter * 2, self.noise_filter * 4, 4, 2, 1, bias=False),
+        self.layer3 = nn.Sequential(nn.Conv2d(self.noise_filter * 2, self.noise_filter * 4,
+                                              kernel_size=4, stride=2, padding=1, bias=False),
                                     nn.BatchNorm2d(self.noise_filter * 4),
                                     nn.LeakyReLU(0.2, inplace=True))
 
-        self.layer4 = nn.Sequential(nn.Conv2d(self.noise_filter * 4, self.noise_filter * 8, 4, 2, 1, bias=False),
+        self.layer4 = nn.Sequential(nn.Conv2d(self.noise_filter * 4, self.noise_filter * 8,
+                                              kernel_size=4, stride=2, padding=1, bias=False),
                                     nn.BatchNorm2d(self.noise_filter * 8),
                                     nn.Sigmoid())
 
-        self.layer5 = nn.Sequential(nn.Conv2d(self.noise_filter * 8, 1, 4, 1, 0, bias=False),
+        self.layer5 = nn.Sequential(nn.Conv2d(self.noise_filter * 8, 1,
+                                              kernel_size=4, stride=1, padding=0, bias=False),
                                     nn.Sigmoid()
                                     )
 
