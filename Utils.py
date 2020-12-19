0259d094cdc110341f5f43ad534067a12a435481
RaptorJung <jk96491@naver.com>
2020-12-19 15:10:26 +0900

    데이터 저장 위치 수정

--- Utils.py
+++ Utils.py
@@ -14,10 +14,10 @@ def MnistLoadData(image_size, batch_size, train, generate_image):
     else:
         transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
 
-    os.makedirs("../Data/mnist", exist_ok=True)
+    os.makedirs("../../Data/mnist", exist_ok=True)
     dataloader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            "../Data/mnist",
+            "../../Data/mnist",
             train=train,
             download=True,
             transform=transform),
@@ -33,7 +33,7 @@ def CIFARLoadData(batch_size, Train, generate_image):
         os.makedirs("images", exist_ok=True)
 
     transform = transforms.Compose([transforms.Scale(64), transforms.ToTensor(), transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
-    dataset = datasets.CIFAR10(root='../Data/CIFAR/', train=Train, download=True, transform=transform)
+    dataset = datasets.CIFAR10(root='../../Data/CIFAR/', train=Train, download=True, transform=transform)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     return data_loader
