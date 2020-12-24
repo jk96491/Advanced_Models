c57fa17241d9ac6a9834aed0236e7b978a7d82cc
RaptorJung <jk96491@naver.com>
2020-12-24 11:53:50 +0900

    MHA 기법 추가

--- Models/SAGAN_Model/Generator.py
+++ Models/SAGAN_Model/Generator.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from Modules.self_attention_Layer import self_attention
 from Modules.multi_head_attention_Layer import multi_head_attention
 
+
 class Generator(nn.Module):
     def __init__(self, args):
         super(Generator, self).__init__()
@@ -28,9 +29,9 @@ class Generator(nn.Module):
         self.layer5 = nn.Sequential(nn.ConvTranspose2d(self.noise_filter, self.channels, 4, 2, 1, bias=False),
                                     nn.Tanh())
 
-        #self.self_attention_layer = multi_head_attention(self.noise_filter, 8)
+        self.self_attention_layer = multi_head_attention(self.noise_filter, 4)
 
-        self.self_attention_layer = self_attention(self.noise_filter)
+        #self.self_attention_layer = self_attention(self.noise_filter)
 
     def forward(self, noise):
         x = self.layer1(noise)
