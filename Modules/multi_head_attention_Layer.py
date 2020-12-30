32ca79899d11b02e6adff678b78f1062c04f3b11
RaptorJung <jk96491@naver.com>
2020-12-30 20:10:03 +0900

    multi_head_attention 수정

--- Modules/multi_head_attention_Layer.py
+++ Modules/multi_head_attention_Layer.py
@@ -9,11 +9,16 @@ class multi_head_attention(nn.Module):
         self.n_head = n_head
         self.out_dim = input_dim // 2
 
-        self.query = nn.Conv2d(input_dim, input_dim // 2 * n_head, kernel_size=1)
-        self.key = nn.Conv2d(input_dim, input_dim // 2 * n_head, kernel_size=1)
-        self.value = nn.Conv2d(input_dim, input_dim * n_head, kernel_size=1)
+        self.query_layers = nn.ModuleList()
+        self.key_layers = nn.ModuleList()
+        self.value_layers = nn.ModuleList()
 
-        self.fc = nn.Linear(input_dim * input_dim, self.out_dim * self.out_dim)
+        for i in range(self.n_head):
+            self.query_layers.append(nn.Conv2d(input_dim, input_dim // 2 , kernel_size=1))
+            self.key_layers.append(nn.Conv2d(input_dim, input_dim // 2, kernel_size=1))
+            self.value_layers.append(nn.Conv2d(input_dim, input_dim, kernel_size=1))
+
+        self.fc = nn.Linear(self.out_dim * self.out_dim * self.n_head, self.out_dim * self.out_dim)
 
         self.scaled_dot_product_attention_layer = scaled_dot_product_attention(input_dim // 2)
 
@@ -23,13 +28,24 @@ class multi_head_attention(nn.Module):
     def forward(self, inputs):
         batch_size, channels, width, height = inputs.size()
 
-        query = self.query(inputs).view(batch_size, -1, width * height * self.n_head).permute(0, 2, 1)
-        key = self.key(inputs).view(batch_size, -1, width * height * self.n_head)
-        value = self.value(inputs).view(batch_size, -1, width * height * self.n_head)
+        attention_output = []
+        attention_score_output = []
 
-        output, attention_score = self.scaled_dot_product_attention_layer(inputs, query, key, value)
-        output = self.fc(output)
+        for i in range(self.n_head):
+            query = self.query_layers[i](inputs).view(batch_size, -1, width * height).permute(0, 2, 1)
+            key = self.key_layers[i](inputs).view(batch_size, -1, width * height)
+            value = self.value_layers[i](inputs).view(batch_size, -1, width * height)
+
+            output, attention_score = self.scaled_dot_product_attention_layer(inputs, query, key, value)
+
+            attention_output.append(output)
+            attention_score_output.append(attention_score)
+
+        attention_output = torch.cat(attention_output, dim=-1)
+        attention_score_output = torch.cat(attention_score_output, dim=-1)
+
+        output = self.fc(attention_output)
         output = output.view(batch_size, channels, self.out_dim, self.out_dim)
         output = self.gamma * output + inputs
 
-        return output, attention_score
+        return output, attention_score_output
